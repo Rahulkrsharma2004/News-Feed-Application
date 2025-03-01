adb097baf686bc53bab1181d330a61ce7f672fe5
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2025-03-02 00:25:31 +0530

    docker setup and deployment

--- Frontend-News/src/App.jsx
+++ Frontend-News/src/App.jsx
@@ -5,7 +5,7 @@ import { io } from "socket.io-client";
 import NewsList from "./components/NewsList";
 import CategorySelector from "./components/CategorySelector";
 
-const socket = io("http://localhost:5000");
+const socket = io("http://backend-service:5000");
 
 const App = () => {
   const dispatch = useDispatch();
